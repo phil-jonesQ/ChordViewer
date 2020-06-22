dcf9902dfe0d6e04ea5ee237e7e45e1e3972c75a
philipjones <phil.jones.24.4@gmail.com>
2020-06-22 21:28:05 +0100

    Change layout..

--- web/index.php
+++ web/index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-
-
-	<link rel=”stylesheet” href=”css/bootstrap.css”>
-	<link rel=”stylesheet” href=”css/bootstrap-responsive.css”>
-	<meta name=”viewport” content=”width=device-width, initial-scale=1.0">
-	<h1>Hello, Chord Viewer</h1>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
    </head>
 
 	<body>
+	<div class="jumbotron text-center">
+	<h1>Chord Viewer</h1>
+	<h2>Select A Song</h2>
+	<h2>The following are songs with lyrics and the chord above each lyric - Click on a link to view the song...</h2>
 	<?php
 
 	// Function to get the meta files, extra the details and return an associated array
@@ -74,13 +77,16 @@ array_multisort($meta_files);
 
 // Generate the dynamic links
 foreach ($meta_files as $title => $file_name) {
+	echo '<h2>';
 	echo '<a href="assets_html/' . $file_name . '">' . $title . '</a>';
-        echo '<br>';
+	echo '<br>';
+	echo '</h2>';
 }
 
 ?>
 
 
+	</div>
 	</body>
 </html>
 
